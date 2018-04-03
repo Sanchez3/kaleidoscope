b98d1cb5059a38c10c4a5d47b3205ddf3a608601
硫酸 <lsch001@163.com>
2018-04-03 17:53:16 +0800

    :tada: Initial Commit

--- src/assets/js/states/state1.js
+++ src/assets/js/states/state1.js
@@ -20,7 +20,7 @@
             this.radiusX = wWidth / 2;
             this.radiusY = wHeight / 2;
             this.slices = 16;
-            this.image = this.make.image(0, 0, 'p1');
+            this.image = this.make.image(0, 0, 'p2');
             console.log(this.image.height)
             var slices = this.slices;
             for (var i = 0; slices >= 0 ? slices >= i : i >= slices; s = slices >= 0 ? ++i : --i) {
@@ -30,7 +30,7 @@
                 this.piece[i].centerX = this.radiusX;
                 this.piece[i].centerY = this.radiusY;
                 this.arc.push(this.add.graphics(0, 0));
-                this._sprite.push(this.add.tileSprite(0, 0, 5 * wWidth, 5 * wWidth, 'p2'));
+                this._sprite.push(this.add.tileSprite(0, 0, 3 * wWidth, 3 * wWidth, 'p2'));
                 this.piece[i].addChild(this._sprite[i]);
                 this.piece[i].addChild(this.arc[i]);
                 this.group.addChild(this.piece[i]);
@@ -45,14 +45,21 @@
                 this._sprite[i].tileScale.x = .5
                 this._sprite[i].tileScale.y = [.5, .5][s % 2];
                 this._sprite[i].position.y = wHeight / 2 - this.image.height / 2;
-                if(s%2){
-                    this.piece[i].scale.setTo(-1, 1)
+                if (s % 2) {
+                    this.piece[i].scale.setTo(-1, 1);
+                    this._sprite[i].position.x = 0;
                 }
             }
             for (var j = this._sprite.length - 1; j >= 0; j--) {
-                TweenLite.to(this._sprite[j], 20, { x: -800 ,y:-200,angle:-100,onUpdate:function(){
-                    console.log()
-                }});
+                this._sprite[j].angle=-15
+                TweenMax.to(this._sprite[j], 10, {
+                    yoyo: true,
+                    repeat:-1,
+                    x: -500,
+                    y: -200,
+                    angle: -55,
+                    onUpdate: function() {}
+                });
             }
         },
         update: function() {}
