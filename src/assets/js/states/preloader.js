5c02a6548738e9f7ca081850f43d469768da8b55
硫酸 <lsch001@163.com>
2018-04-03 17:23:06 +0800

    Initial commit

---
+++ src/assets/js/states/preloader.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Sanchez
+ */
+(function() {
+    'use strict';
+    var Preloader = function() {};
+
+    module.exports = Preloader;
+
+    Preloader.prototype = {
+        loadResources: function() {
+            this.load.image('p1', '../assets/img/p1.jpg');
+            this.load.image('p2', '../assets/img/p2.jpg');
+            this.load.image('p3', '../assets/img/p3.jpg');
+            this.load.start();
+
+        },
+        drawPieProgress: function(_progress) {
+            var that = this;
+            that.pgGraphics.clear();
+            that.pgGraphics.lineStyle(6, 0x29ABE2);
+            that.pgGraphics.arc(this.game.width / 2, this.game.height / 2, 45, this.game.math.degToRad(270), this.game.math.degToRad(360 * _progress / 100 + 270), false);
+            that.pgGraphics.endFill();
+        },
+        create: function() {
+            var that = this;
+            that.pgGraphics = this.add.graphics(0, 0);
+            var style = {
+                fontStyle: 'italic',
+                font: '"Helvetica Neue",Helvetica,Arial,"Microsoft Yahei","Hiragino Sans GB","Heiti SC","WenQuanYi Micro Hei"',
+                fontSize: 19,
+                fill: '#000',
+                align: 'center'
+            };
+            that.progress = this.add.text(this.game.width / 2, this.game.height / 2 + 5, '0%', style);
+            that.progress.anchor.setTo(0.5);
+            this.load.onFileComplete.add(that.onfileComplete, this);
+            this.load.onLoadComplete.addOnce(that.onLoadComplete, this);
+            this.loadResources();
+
+        },
+        onLoadComplete: function() {
+            var that = this;
+            that.game.state.start('State1');
+            return;
+
+        },
+        onfileComplete: function(progress, cacheKey, success, totalLoaded, totalFiles) {
+            var that = this;
+            that.drawPieProgress(progress);
+            that.progress.text = progress + '%';
+        }
+    };
+
+}());
---
+++ src/assets/js/states/state1.js
@@ -0,0 +1,62 @@
+/**
+ * Created by Sanchez
+ */
+(function() {
+    'use strict';
+    var State1 = function() {};
+    module.exports = State1;
+    State1.prototype = {
+
+        preload: function() {},
+        create: function() {
+            this.piece = [];
+            this._sprite = [];
+            this.arc = [];
+            this.group = this.add.group();
+            var s = 0;
+            var _angle = 0;
+            var wWidth = window.innerWidth;
+            var wHeight = window.innerHeight;
+            this.radiusX = wWidth / 2;
+            this.radiusY = wHeight / 2;
+            this.slices = 16;
+            this.image = this.make.image(0, 0, 'p1');
+            console.log(this.image.height)
+            var slices = this.slices;
+            for (var i = 0; slices >= 0 ? slices >= i : i >= slices; s = slices >= 0 ? ++i : --i) {
+                _angle = 2 * Math.PI / this.slices;
+                this.piece.push(this.add.sprite(0, 0));
+                this.piece[i].anchor.setTo(0.5);
+                this.piece[i].centerX = this.radiusX;
+                this.piece[i].centerY = this.radiusY;
+                this.arc.push(this.add.graphics(0, 0));
+                this._sprite.push(this.add.tileSprite(0, 0, 5 * wWidth, 5 * wWidth, 'p2'));
+                this.piece[i].addChild(this._sprite[i]);
+                this.piece[i].addChild(this.arc[i]);
+                this.group.addChild(this.piece[i]);
+                this.arc[i].beginFill(0x000);
+                //x,y,r,sAngle,eAngle,counterclockwise
+                this.arc[i].moveTo(0, 0);
+                this.arc[i].arc(0, 0, 100 * this.radiusX, _angle * -0.51, _angle * 0.51);
+                this.arc[i].lineTo(0, 0);
+                this.piece[i].rotation = s * _angle;
+                this._sprite[i].mask = this.arc[i];
+                // TweenLite.to(this._sprite[g], 0.5, { x: -100 });
+                this._sprite[i].tileScale.x = .5
+                this._sprite[i].tileScale.y = [.5, .5][s % 2];
+                this._sprite[i].position.y = wHeight / 2 - this.image.height / 2;
+                if(s%2){
+                    this.piece[i].scale.setTo(-1, 1)
+                }
+            }
+            for (var j = this._sprite.length - 1; j >= 0; j--) {
+                TweenLite.to(this._sprite[j], 20, { x: -800 ,y:-200,angle:-100,onUpdate:function(){
+                    console.log()
+                }});
+            }
+        },
+        update: function() {}
+
+    };
+
+}());
