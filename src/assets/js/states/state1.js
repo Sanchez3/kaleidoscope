f3bf96743d572e4b261aad20f5a970e88bdcd2e4
硫酸 <lsch001@163.com>
2018-04-09 12:03:03 +0800

    :construction: Fixing

--- src/assets/js/states/state1.js
+++ src/assets/js/states/state1.js
@@ -30,7 +30,7 @@
                 this.piece[i].centerX = this.radiusX;
                 this.piece[i].centerY = this.radiusY;
                 this.arc.push(this.add.graphics(0, 0));
-                this._sprite.push(this.add.tileSprite(0, 0, 3 * wWidth, 3 * wWidth, 'p2'));
+                this._sprite.push(this.add.tileSprite(0, 0, 5 * wWidth, 5 * wWidth, 'p2'));
                 this.piece[i].addChild(this._sprite[i]);
                 this.piece[i].addChild(this.arc[i]);
                 this.group.addChild(this.piece[i]);
@@ -47,9 +47,10 @@
                 this._sprite[i].position.y = wHeight / 2 - this.image.height / 2;
                 if (s % 2) {
                     this.piece[i].scale.setTo(-1, 1);
-                    this._sprite[i].position.x = 0;
+                    // this._sprite[i].position.x = 0;
                 }
             }
+            this.offset = 0;
             this.input.onDown.add(this.tap, this);
             this.input.onUp.add(this.release, this);
             this.input.addMoveCallback(this.drag, this);
@@ -73,14 +74,17 @@
             this.pressed = true;
             this.reference = this.game.input.x;
             this.timestamp = Date.now();
+    
+
 
         },
         drag: function(pointer, x, y) {
             if (this.pressed) {
+                console.log(this.reference)
                 var delta = this.reference - x;
                 if (delta > 2 || delta < -2) {
-                    this.reference = x;
-                    this._draw(this.offset + delta);
+                    // this.reference = x;
+                    this._draw(delta+this.offset);
                 }
             }
         },
@@ -91,7 +95,7 @@
             dy = x / that.game.width * that.game.height;
 
             for (var j = this._sprite.length - 1; j >= 0; j--) {
-                TweenMax.set(this._sprite[j], { x: dx, y: dy })
+                TweenMax.to(this._sprite[j], 0,{ x: '+='+dx, y: '+='+dy })
             }
 
         },
